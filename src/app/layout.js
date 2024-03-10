20fdb6496193989cc2e3ddca1e0f776b0693939b
xxx <xnatthawut1990@gmail.com>
2024-03-10 17:59:23 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -73,21 +73,13 @@ export default function RootLayout({ children }) {
 
 
 
- <script dangerouslySetInnerHTML={{ 
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N27Q7BXD');`,
-          }} />
 
 
         </Head>
 
         <body className={inter.className}>
 
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N27Q7BXD"
-            height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+      
         <Nav/>
     
 
